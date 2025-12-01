25018c1951672b9b859f43be69b4b8acde8c61f0
Srideep <srideepmaulik@gmail.com>
2025-12-01 20:21:11 +0000

    add include/ msp432p401r.lds src/

---
+++ src/sources.mk
@@ -0,0 +1,22 @@
+#******************************************************************************
+# Copyright (C) 2017 by Alex Fosdick - University of Colorado
+#
+# Redistribution, modification or use of this software in source or binary
+# forms is permitted as long as the files maintain this copyright. Users are 
+# permitted to modify this and use it to learn about the field of embedded
+# software. Alex Fosdick and the University of Colorado are not liable for any
+# misuse of this material. 
+#
+#*****************************************************************************
+PLATFORM ?= HOST
+ifeq ($(PLATFORM), HOST)
+SOURCES = \
+	main.c \
+	memory.c
+INCLUDES = -Iinclude/common 
+else ifeq ($(PLATFORM),MSP432)
+SOURCES = \
+	main.c \
+	memory.c
+	
+endif
